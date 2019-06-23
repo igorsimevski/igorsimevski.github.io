956bccfff3617e4c65543b2bab984610a8b4a3fc
igor <i.simevski@gmail.com>
2019-06-23 20:30:07 +0200

    scroll fix

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
     // Page scrolling feature
-    // $('a.page-scroll').bind('click', function(event) {
-    //     var link = $(this);
-    //     $('html, body').stop().animate({
-    //         scrollTop: $(link.attr('href')).offset().top - 50
-    //     }, 500);
-    //     event.preventDefault();
-    //     $("#navbar").collapse('hide');
-    // });
+    $('a.page-scroll').bind('click', function(event) {
+        var link = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(link.attr('href')).offset().top - 50
+        }, 500);
+        event.preventDefault();
+        $("#navbar").collapse('hide');
+    });
 });
 
 var cbpAnimatedHeader = (function() {
