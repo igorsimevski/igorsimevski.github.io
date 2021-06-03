b32328a93c5e0c49e49225466f94fbedb9edbd1a
Igor Simevski <igorsimevski@users.noreply.github.com>
2021-06-03 21:43:52 +0200

    upgraded chart.js

--- build/files.conf.js
+++ build/files.conf.js
@@ -8,7 +8,7 @@ module.exports = {
             'node_modules/jquery/dist/jquery.js',
             'static/js/bs3-typeahead.js',
             'node_modules/bootstrap/dist/js/bootstrap.js',
-            'node_modules/chart.js/dist/Chart.js',
+            'node_modules/chart.js/dist/chart.js',
             'node_modules/pace-progress/pace.js',
             'node_modules/wowjs/dist/wow.js',
             'node_modules/lozad/dist/lozad.js',
